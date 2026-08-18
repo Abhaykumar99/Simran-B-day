b507687b27c4c881ac65362b036e54f9b844a684
Abhay Kumar <krabhi4201@gmail.com>
2026-08-18 22:20:39 +0530

    Update wish audio to an 18-year-old male friend's voice using hi-IN-MadhurNeural

--- generate_wish.py
+++ generate_wish.py
@@ -1,17 +1,17 @@
 import asyncio
 import edge_tts
 
-# 18-year-old friend voice: natural pitch, warm slow pace. No "didi".
+# 18-year-old boy (friend) voice: natural pitch, warm slow pace. 
 # Commas (,) and ellipses (...) create natural pauses.
 
 PLAIN_TEXT = "Hey Simran! ... Happy Birthday to you! ... Aaj tera din hai, aur hum chahte hain, ki tujhe pata chale tu kitni special hai hamare liye. ... Teri smile, tera energy, sab kuch, we love it all! ... Bahut sara pyar, aur dher saari khushiyaan tujhe! ... Happy Birthday Simran! ... We love you so much!"
 
 async def main():
-    print("Generating 18-year-old friend birthday wish...")
+    print("Generating 18-year-old boy birthday wish...")
     communicate = edge_tts.Communicate(
         text=PLAIN_TEXT, 
-        voice="hi-IN-SwaraNeural",
-        rate="-15%",
+        voice="hi-IN-MadhurNeural",
+        rate="-12%",
         pitch="+5Hz"
     )
     await communicate.save("build/audio/wish.mp3")
