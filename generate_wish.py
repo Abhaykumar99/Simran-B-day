39ae5b78db29290e4ec04e89601c5a8a643a6958
Abhay Kumar <krabhi4201@gmail.com>
2026-08-18 22:22:04 +0530

    Add phonetic spelling 'Simraan' to add emotion and effort to the TTS output

--- generate_wish.py
+++ generate_wish.py
@@ -4,7 +4,7 @@ import edge_tts
 # 18-year-old boy (friend) voice: natural pitch, warm slow pace. 
 # Commas (,) and ellipses (...) create natural pauses.
 
-PLAIN_TEXT = "Hey Simran! ... Happy Birthday to you! ... Aaj tera din hai, aur hum chahte hain, ki tujhe pata chale tu kitni special hai hamare liye. ... Teri smile, tera energy, sab kuch, we love it all! ... Bahut sara pyar, aur dher saari khushiyaan tujhe! ... Happy Birthday Simran! ... We love you so much!"
+PLAIN_TEXT = "Hey, Simraan! ... Happy Birthday to you! ... Aaj tera din hai, aur hum chahte hain, ki tujhe pata chale tu kitni special hai hamare liye. ... Teri smile, tera energy, sab kuch, we love it all! ... Bahut sara pyar, aur dher saari khushiyaan tujhe! ... Happy Birthday, Simraan! ... We love you so much!"
 
 async def main():
     print("Generating 18-year-old boy birthday wish...")
