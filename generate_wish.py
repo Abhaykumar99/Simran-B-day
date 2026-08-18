c8b4fc382787bc6fade52e22d92b4703e7c4d77b
Abhay Kumar <krabhi4201@gmail.com>
2026-08-18 22:56:14 +0530

    Switch to NeerjaNeural (Indian English) voice for much more natural emotion and pronunciation

--- generate_wish.py
+++ generate_wish.py
@@ -1,18 +1,17 @@
 import asyncio
 import edge_tts
 
-# Sweet, warm female voice (like a close girl friend): natural pitch, warm slow pace. 
-# Commas (,) and ellipses (...) create natural pauses, phonetic 'Simraan' for emphasis.
+# Using en-IN-NeerjaNeural: an Indian English female voice which usually has more dynamic intonation and natural emotion for English/Hinglish than the Hindi voice.
 
 PLAIN_TEXT = "Hey, Simran! ... Happy Birthday to you! ... Aaj tera din hai, aur hum chahte hain, ki tujhe pata chale tu kitni special hai hamare liye. ... Teri smile, tera energy, sab kuch, we love it all! ... Bahut sara pyar, aur dher saari khushiyaan tujhe! ... Happy Birthday, Simran! ... We love you so much!"
 
 async def main():
-    print("Generating sweet female birthday wish...")
+    print("Generating expressive female birthday wish...")
     communicate = edge_tts.Communicate(
         text=PLAIN_TEXT, 
-        voice="hi-IN-SwaraNeural",
+        voice="en-IN-NeerjaNeural",  # Switched to Indian English voice for better emotion
         rate="-15%",
-        pitch="+8Hz"
+        pitch="+5Hz"
     )
     await communicate.save("build/audio/wish.mp3")
     print("Done! Saved to build/audio/wish.mp3")
