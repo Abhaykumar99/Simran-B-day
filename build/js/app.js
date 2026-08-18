3d3020272a433118d00948362bb08aca0b3fd270
Abhay Kumar <krabhi4201@gmail.com>
2026-08-19 00:24:37 +0530

    Update countdown time to 12:26 AM

--- build/js/app.js
+++ build/js/app.js
@@ -55,7 +55,7 @@
     fireworksAlreadySeen = false;
   }
 
-  var targetDate = new Date('2026-08-19T00:20:00+05:30').getTime(); // 19 Aug 12:20 AM
+  var targetDate = new Date('2026-08-19T00:26:00+05:30').getTime(); // 19 Aug 12:26 AM
   var countdownOverlay = document.getElementById('countdownOverlay');
   var cdDays = document.getElementById('cd-days');
   var cdHours = document.getElementById('cd-hours');
