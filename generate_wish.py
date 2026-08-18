108125ceef8a93a44996de58d14a88a586d0114b
Abhay Kumar <krabhi4201@gmail.com>
2026-08-18 22:48:48 +0530

    Fix 'Simran' pronunciation by reverting phonetic spelling

--- generate_wish.py
+++ generate_wish.py
@@ -4,7 +4,7 @@ import edge_tts
 # Sweet, warm female voice (like a close girl friend): natural pitch, warm slow pace. 
 # Commas (,) and ellipses (...) create natural pauses, phonetic 'Simraan' for emphasis.
 
-PLAIN_TEXT = "Hey, Simraan! ... Happy Birthday to you! ... Aaj tera din hai, aur hum chahte hain, ki tujhe pata chale tu kitni special hai hamare liye. ... Teri smile, tera energy, sab kuch, we love it all! ... Bahut sara pyar, aur dher saari khushiyaan tujhe! ... Happy Birthday, Simraan! ... We love you so much!"
+PLAIN_TEXT = "Hey, Simran! ... Happy Birthday to you! ... Aaj tera din hai, aur hum chahte hain, ki tujhe pata chale tu kitni special hai hamare liye. ... Teri smile, tera energy, sab kuch, we love it all! ... Bahut sara pyar, aur dher saari khushiyaan tujhe! ... Happy Birthday, Simran! ... We love you so much!"
 
 async def main():
     print("Generating sweet female birthday wish...")
