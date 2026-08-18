cd93c7a03bc29544299e29768613ec954390a007
Abhay Kumar <krabhi4201@gmail.com>
2026-08-18 23:10:52 +0530

    Update TTS spelling to 'Simrun' for correct Hindi pronunciation

--- generate_wish.py
+++ generate_wish.py
@@ -4,7 +4,7 @@ import edge_tts
 # Sweet, warm female voice (Indian Hindi): natural pitch, warm slow pace. 
 # Commas (,) and ellipses (...) create natural pauses.
 
-PLAIN_TEXT = "Hey, Simran! ... Happy Birthday to you! ... Aaj tera din hai, aur hum chahte hain, ki tujhe pata chale tu kitni special hai hamare liye. ... Teri smile, tera energy, sab kuch, we love it all! ... Bahut sara pyar, aur dher saari khushiyaan tujhe! ... Happy Birthday, Simran! ... We love you so much!"
+PLAIN_TEXT = "Hey, Simrun! ... Happy Birthday to you! ... Aaj tera din hai, aur hum chahte hain, ki tujhe pata chale tu kitni special hai hamare liye. ... Teri smile, tera energy, sab kuch, we love it all! ... Bahut sara pyar, aur dher saari khushiyaan tujhe! ... Happy Birthday, Simrun! ... We love you so much!"
 
 async def main():
     print("Generating Indian Hindi female birthday wish...")
